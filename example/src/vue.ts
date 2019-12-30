b2598c2415aa924fc886b9f4af474d152a23f656
myWsq <wsq961@outlook.com>
2019-12-30 18:14:55 +0800

    feat(example): 修改变量名

--- example/src/vue.ts
+++ example/src/vue.ts
@@ -1,27 +1,27 @@
-import List from "../../src/query-it";
+import QueryIt from "../../src/query-it";
 import { reactive, ref } from "@vue/composition-api";
 
 export default function useQueryIt<T>(wait: number = 0) {
-  const list = new List<T>(() => {
-    items.value = list.items;
-    currentPage.value = list.currentPage;
-    pageSize.value = list.pageSize;
-    pageCount.value = list.pageCount;
-    total.value = list.total;
+  const query = new QueryIt<T>(() => {
+    items.value = query.items;
+    currentPage.value = query.currentPage;
+    pageSize.value = query.pageSize;
+    pageCount.value = query.pageCount;
+    total.value = query.total;
   }, wait);
-  const items = ref(list.items);
-  const currentPage = ref(list.currentPage);
-  const pageSize = ref(list.pageSize);
-  const pageCount = ref(list.pageCount);
-  const total = ref(list.total);
+  const items = ref(query.items);
+  const currentPage = ref(query.currentPage);
+  const pageSize = ref(query.pageSize);
+  const pageCount = ref(query.pageCount);
+  const total = ref(query.total);
 
   return reactive({
-    load: list.load.bind(list),
-    sort: list.sort.bind(list),
-    filter: list.filter.bind(list),
-    search: list.search.bind(list),
-    setPageSize: list.setPageSize.bind(list),
-    setCurrentPage: list.setCurrentPage.bind(list),
+    load: query.load.bind(query),
+    sort: query.sort.bind(query),
+    filter: query.filter.bind(query),
+    search: query.search.bind(query),
+    setPageSize: query.setPageSize.bind(query),
+    setCurrentPage: query.setCurrentPage.bind(query),
     items,
     currentPage,
     pageSize,
