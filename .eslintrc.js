6ece01cd4032e5b26311d7a14329fc2398c097b7
myWsq <wsq961@outlook.com>
2020-05-31 09:53:47 +0800

    chore: 更新技术栈, 添加基础配置

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+  ],
+};
---
+++ src/core.ts
@@ -0,0 +1,40 @@
+type DataHandler<T> = (data: T[]) => void;
+
+export function createQuery<T>() {
+  let data: T[] = [];
+  const r = {
+    load,
+    search,
+    filter,
+    sort,
+    data,
+  };
+
+  let _filter: DataHandler<T>;
+  let _search: DataHandler<T>;
+  let _sort: DataHandler<T>;
+
+  function load(source: T[]) {
+    data = source.concat();
+    return r;
+  }
+
+  function filter(val: any) {
+    const arr = data.concat();
+    _filter = (d) => {
+      console.log(d, val);
+    };
+    _sort && _sort(arr);
+    return r;
+  }
+
+  function search() {
+    return r;
+  }
+
+  function sort() {
+    return r;
+  }
+
+  return r;
+}
