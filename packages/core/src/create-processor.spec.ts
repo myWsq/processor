7538d70b558fec4d53bcd086d2cdb94ad47919ce
myWsq <wsq961@outlook.com>
2020-06-01 23:41:26 +0800

    ref(core): 修复类型不提示的问题, 添加 debounce

--- packages/core/src/create-processor.spec.ts
+++ packages/core/src/create-processor.spec.ts
@@ -7,119 +7,139 @@ const data = [
 ];
 
 test("load", () => {
-  const query = createProcessor();
-  query.load(data);
-  expect(query.exec().current).toEqual(data);
+  const processor = createProcessor();
+  processor.load(data);
+  expect(processor.exec().current).toEqual(data);
 });
 
 describe("Baisc filter", () => {
-  const query = createProcessor(data);
+  const processor = createProcessor(data);
 
   test("single field with single condition", () => {
-    query.filter({ name: "test0" });
-    expect(query.exec().current).toEqual([data[1]]);
+    processor.filter({ name: "test0" });
+    expect(processor.exec().current).toEqual([data[1]]);
   });
 
   test("single field with multiple condition", () => {
-    query.filter({ name: ["test0", "test1"] });
-    expect(query.exec().current).toEqual([data[0], data[1], data[3]]);
+    processor.filter({ name: ["test0", "test1"] });
+    expect(processor.exec().current).toEqual([data[0], data[1], data[3]]);
   });
 
   test("multiple field with single condition", () => {
-    query.filter({ name: "test0", age: 16 });
-    expect(query.exec().current).toEqual([]);
+    processor.filter({ name: "test0", age: 16 });
+    expect(processor.exec().current).toEqual([]);
   });
 
   test("multiple field with multiple conditions", () => {
-    query.filter({ name: ["test0", "test2"], age: [11, 14] });
-    expect(query.exec().current).toEqual([data[2]]);
+    processor.filter({ name: ["test0", "test2"], age: [11, 14] });
+    expect(processor.exec().current).toEqual([data[2]]);
   });
 
   test("with callback function", () => {
-    query.filter({ name: ["test0", "test1"], age: (val) => val > 17 });
-    expect(query.exec().current).toEqual([data[1]]);
+    processor.filter({ name: ["test0", "test1"], age: (val) => val > 17 });
+    expect(processor.exec().current).toEqual([data[1]]);
   });
 });
 
 describe("Basic search", () => {
-  const query = createProcessor(data);
+  const processor = createProcessor(data);
 
   test("search text on all of fields", () => {
-    query.search("test0");
-    expect(query.exec().current).toEqual([data[1]]);
+    processor.search("test0");
+    expect(processor.exec().current).toEqual([data[1]]);
   });
 
   test("search text on some fields", () => {
-    query.search("test0", ["age"]);
-    expect(query.exec().current).toEqual([]);
+    processor.search("test0", ["age"]);
+    expect(processor.exec().current).toEqual([]);
   });
 
   test("search by callback function", () => {
-    query.search((val) => {
+    processor.search((val) => {
       return [val[0]];
     });
-    expect(query.exec().current).toEqual([data[0]]);
+    expect(processor.exec().current).toEqual([data[0]]);
   });
 });
 
 describe("Basic sort", () => {
-  const query = createProcessor(data);
+  const processor = createProcessor(data);
 
   test("single field asc", () => {
-    query.sort("name");
-    expect(query.exec().current).toEqual([data[1], data[0], data[3], data[2]]);
+    processor.sort("name");
+    expect(processor.exec().current).toEqual([
+      data[1],
+      data[0],
+      data[3],
+      data[2],
+    ]);
   });
 
   test("single field desc", () => {
-    query.sort("name", "desc");
-    expect(query.exec().current).toEqual([data[2], data[0], data[3], data[1]]);
+    processor.sort("name", "desc");
+    expect(processor.exec().current).toEqual([
+      data[2],
+      data[0],
+      data[3],
+      data[1],
+    ]);
   });
 
   test("multiple fields asc", () => {
-    query.sort(["name", "age"]);
-    expect(query.exec().current).toEqual([data[1], data[3], data[0], data[2]]);
+    processor.sort(["name", "age"]);
+    expect(processor.exec().current).toEqual([
+      data[1],
+      data[3],
+      data[0],
+      data[2],
+    ]);
   });
 
   test("multiple fields with different orders", () => {
-    query.sort(["name", "age"], ["acs", "desc"]);
-    expect(query.exec().current).toEqual([data[1], data[0], data[3], data[2]]);
+    processor.sort(["name", "age"], ["acs", "desc"]);
+    expect(processor.exec().current).toEqual([
+      data[1],
+      data[0],
+      data[3],
+      data[2],
+    ]);
   });
 });
 
 describe("Basic page", () => {
-  const query = createProcessor(data);
+  const processor = createProcessor(data);
 
   test("paging", () => {
-    query.page(2, 1);
-    expect(query.exec().current).toEqual([data[1]]);
-    expect(query.exec().page).toEqual(2);
-    expect(query.exec().pageCount).toEqual(4);
-    expect(query.exec().total).toEqual(4);
+    processor.page(1, 2);
+    expect(processor.exec().current).toEqual([data[1]]);
+    expect(processor.exec().page).toEqual(2);
+    expect(processor.exec().pageCount).toEqual(4);
+    expect(processor.exec().total).toEqual(4);
   });
 
-  test("invalid page", () => {
-    query.page(3, -1);
-    expect(query.exec().page).toEqual(1);
-    expect(query.exec().pageCount).toEqual(1);
+  test("all data", () => {
+    processor.page();
+    expect(processor.exec().page).toEqual(1);
+    expect(processor.exec().pageCount).toEqual(1);
   });
 });
 
 describe("Chain", () => {
   test("filter and search", () => {
-    const query = createProcessor(data);
-    query.filter({ name: "test1" }).search("11");
-    expect(query.exec().current).toEqual([data[3]]);
+    const processor = createProcessor(data);
+    processor.filter({ name: "test1" }).search("11");
+    expect(processor.exec().current).toEqual([data[3]]);
   });
 
   test("search and sort", () => {
-    const query = createProcessor(data);
-    query.search("test1").sort("age");
-    expect(query.exec().current).toEqual([data[3], data[0]]);
+    const processor = createProcessor(data);
+    processor.search("test1").sort("age");
+    expect(processor.exec().current).toEqual([data[3], data[0]]);
   });
 
   test("search and sort page", () => {
-    const query = createProcessor(data);
-    query.search("test1").sort("age").page(2, 1);
-    expect(query.exec().current).toEqual([data[0]]);
+    const processor = createProcessor(data);
+    processor.search("test1").sort("age").page(1, 2);
+    expect(processor.exec().current).toEqual([data[0]]);
   });
 });
--- packages/core/src/create-processor.ts
+++ packages/core/src/create-processor.ts
@@ -1,14 +1,101 @@
 // Types
+// --------------------------------
 export type SourceData = Record<string, any>;
 export type OneOrMore<T> = T | T[];
 export type DataHandler<T extends SourceData> = (data: T[]) => T[];
-export type Result<T> = {
+export interface Processor<T> {
+  /**
+   * Load new data.
+   * @param source - New data
+   */
+  load: (source: T[]) => Processor<T>;
+  /**
+   * Search by text on multiple fields. Or you can provide a custom search function.
+   *
+   * @example
+   * processor.search('ttt') // search on all fields
+   * processor.search('ttt', ['name']) // search on 'name' fields
+   * processor.search(data => data.filter(item => item.name === 'ttt')) // custom search function
+   */
+  search: (
+    searcher: string | ((val: T[]) => T[]),
+    fields?: (keyof T)[] | undefined
+  ) => Processor<T>;
+  /**
+   * Filter by a query object
+   *
+   * @example
+   * processor.filter({
+   *   name: ["Paul Lee", "Donald Clark"] // name equal to "Paul Lee" or "Donald Clark"
+   *   sex: "female", // and sex equal to "female"
+   *   age: (val) => val > 18 // and age greater than 18
+   * });
+   */
+  filter: (
+    option: {
+      [key in keyof T]?:
+        | T[key]
+        | ((val: T[key]) => boolean)
+        | (T[key] | ((val: T[key]) => boolean))[];
+    }
+  ) => Processor<T>;
+  /**
+   * Same with [lodash/orderby](https://lodash.com/docs/4.17.15#orderBy). Or provide a custom sort function likes `Array.sort`
+   *
+   * @example
+   * processor.sort("name"); // single field asc
+   * processor.sort("name", "desc"); // single field desc
+   * processor.sort(["name", "age"]); // multiple fields asc
+   * processor.sort(["name", "age"], ["acs", "desc"]); // multiple fields with different orders
+   */
+  sort: (
+    option: keyof T | (keyof T)[] | ((a: T, b: T) => number),
+    order?:
+      | "acs"
+      | "desc"
+      | "ascing"
+      | "descing"
+      | ("acs" | "desc" | "ascing" | "descing")[]
+      | undefined
+  ) => Processor<T>;
+  /**
+   * Page data.
+   * @param size - Page size. Optional, if not provided,
+   *               all data will be returned and the
+   *               current page will be set to 1.
+   *
+   * @param current - Current page. Default 1.
+   */
+  page: (size?: number, current?: number) => Processor<T>;
+
+  /**
+   * Set callback function, it will be called when result update.
+   *
+   * @example
+   * processor.onUpdate(result => {
+   *  console.log(result);
+   * })
+   */
+  onUpdate: (callback: OnUpdateCallback<T>) => Processor<T>;
+  /**
+   * Calculate immediately.
+   */
+  exec: () => Result<T>;
+}
+export interface Result<T> {
+  /** Paged data */
   current: T[];
+  /** Current page */
   page: number;
+  /** Total number of page */
   pageCount: number;
+  /** Total number of data handled by filter, search, sort  */
   total: number;
-};
+}
+export type OnUpdateCallback<T> = (result: Result<T>) => Processor<T>;
+
 // Utils
+// --------------------------------
 function toArray(item: any) {
   return !(item instanceof Array) ? [item] : item;
 }
@@ -25,31 +112,56 @@ function isEqual(target: any, expect: any): boolean {
   }
 }
 
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number,
+  immediate = false
+) {
+  let timeout: number | undefined;
+  return function (this: any, ...args: Parameters<T>) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      timeout = undefined;
+      if (!immediate) func.apply(this, args);
+    }, wait);
+    if (immediate && !timeout) func.apply(this, args);
+  };
+}
+
 // Main
-export function createProcessor<T extends SourceData>(source?: T[]) {
-  const store = {
+// --------------------------------
+/**
+ * Create a processor instance.
+ * @param source - Source data.
+ * @param wait - debounce wating time (ms). Default 10.
+ */
+export function createProcessor<T extends SourceData>(
+  source?: T[],
+  wait = 10
+): Processor<T> {
+  const state = {
     filter: {
+      // cache
       cache: [] as T[],
-      shouldCacheUpdate: true,
-      method: (data: T[]) => data,
+      // default handler
+      handler: (data: T[]) => data,
+      // call this method to update cache
       update() {
-        store.filter.cache = store.filter.method(data);
+        state.filter.cache = state.filter.handler(data); // use origin data
       },
     },
     search: {
       cache: [] as T[],
-      shouldCacheUpdate: true,
-      method: (data: T[]) => data,
+      handler: (data: T[]) => data,
       update() {
-        store.search.cache = store.search.method(store.filter.cache);
+        state.search.cache = state.search.handler(state.filter.cache); // use filter cache
       },
     },
     sort: {
       cache: [] as T[],
-      shouldCacheUpdate: true,
-      method: (data: T[]) => data,
+      handler: (data: T[]) => data,
       update() {
-        store.sort.cache = store.sort.method(store.search.cache);
+        state.sort.cache = state.sort.handler(state.search.cache); // use search cache
       },
     },
     page: {
@@ -59,146 +171,135 @@ export function createProcessor<T extends SourceData>(source?: T[]) {
         pageCount: 0,
         total: 0,
       } as Result<T>,
-      shouldCacheUpdate: true,
-      method: (data: T[]) => ({
+      handler: (data: T[]) => ({
         current: data,
         page: 1,
         pageCount: 1,
         total: data.length,
       }),
       update() {
-        store.page.cache = store.page.method(store.sort.cache);
+        state.page.cache = state.page.handler(state.sort.cache); // use sort cache
       },
     },
   };
+
+  // origin data
   let data: T[] = source || [];
-  const r = {
-    load,
-    search,
-    filter,
-    sort,
-    page,
-    exec,
+  let onUpdateHandler: OnUpdateCallback<T> | null = null;
+
+  // calculate state
+  function calc(cacheKeys: (keyof typeof state)[]) {
+    cacheKeys.forEach((key) => {
+      state[key].update();
+    });
+    onUpdateHandler && onUpdateHandler(state.page.cache);
+  }
+
+  const debounceCalc = debounce(calc, wait);
+
+  // processor instance
+  const processor: Processor<T> = {
+    // load
+    load(source: T[]) {
+      data = source.concat();
+      debounceCalc(["filter", "search", "sort", "page"]);
+      return processor;
+    },
+    // Filter
+    filter(option) {
+      state.filter.handler = (d) => {
+        return d.filter((item) =>
+          Object.keys(option).every((field) => {
+            const target = item[field];
+            const expects = toArray(option[field]);
+            return expects.some((expect) => isEqual(target, expect));
+          })
+        );
+      };
+      debounceCalc(["filter", "search", "sort", "page"]);
+      return processor;
+    },
+    // search
+    search(searcher, fields) {
+      state.search.handler = (d) => {
+        if (!searcher) {
+          return d.concat();
+        }
+        if (typeof searcher === "function") {
+          return searcher(d.concat());
+        }
+        return d.filter((item) => {
+          const searchColumns = fields || Object.keys(item);
+          const regexp = new RegExp(searcher, "i");
+          return searchColumns.some(
+            (column) => toString(item[column]).search(regexp) > -1
+          );
+        });
+      };
+      debounceCalc(["search", "sort", "page"]);
+      return processor;
+    },
+    // sort
+    sort(option, order) {
+      state.sort.handler = (d) => {
+        d = d.concat();
+        if (typeof option === "function") {
+          return d.sort(option);
+        } else {
+          const options = toArray(option);
+          const orders = toArray(order);
+          const sorter = (a: any, b: any) =>
+            options
+              .map((o, i) => {
+                const dir = ["acs", "ascing", undefined].includes(orders[i])
+                  ? 1
+                  : -1;
+                return a[o] > b[o] ? dir : a[o] < b[o] ? -dir : 0;
+              })
+              .reduce((p, n) => (p ? p : n), 0);
+          d.sort(sorter);
+          return d;
+        }
+      };
+      debounceCalc(["sort", "page"]);
+      return processor;
+    },
+    // page
+    page(size, current = 1): typeof processor {
+      state.page.handler = (d) => {
+        if (!size || size <= 0) {
+          return {
+            current: d.concat(),
+            page: 1,
+            pageCount: 1,
+            total: d.length,
+          };
+        }
+        if (current < 1) {
+          current = 1;
+        }
+        const pageCount = Math.ceil(d.length / size);
+        current = Math.min(current, pageCount);
+
+        return {
+          current: d.slice((current - 1) * size, current * size),
+          page: current,
+          total: d.length,
+          pageCount,
+        };
+      };
+      debounceCalc(["page"]);
+      return processor;
+    },
+    onUpdate(func) {
+      onUpdateHandler = func;
+      return processor;
+    },
+    exec() {
+      calc(["filter", "search", "sort", "page"]);
+      return state.page.cache;
+    },
   };
 
-  function load(source: T[]) {
-    data = source.concat();
-    store.filter.shouldCacheUpdate = true;
-    store.search.shouldCacheUpdate = true;
-    store.sort.shouldCacheUpdate = true;
-    store.page.shouldCacheUpdate = true;
-    return r;
-  }
-
-  function filter(
-    option: {
-      [key in keyof T]?: OneOrMore<T[key] | ((val: T[key]) => boolean)>;
-    }
-  ) {
-    store.filter.method = (d) => {
-      return d.filter((item) =>
-        Object.keys(option).every((field) => {
-          const target = item[field];
-          const expects = toArray(option[field]);
-          return expects.some((expect) => isEqual(target, expect));
-        })
-      );
-    };
-    store.filter.shouldCacheUpdate = true;
-    store.search.shouldCacheUpdate = true;
-    store.sort.shouldCacheUpdate = true;
-    store.page.shouldCacheUpdate = true;
-    return r;
-  }
-
-  function search(
-    searcher: string | ((val: T[]) => T[]),
-    fields?: (keyof T)[]
-  ) {
-    store.search.method = (d) => {
-      if (!searcher) {
-        return d.concat();
-      }
-      if (typeof searcher === "function") {
-        return searcher(d.concat());
-      }
-      return d.filter((item) => {
-        const searchColumns = fields || Object.keys(item);
-        const regexp = new RegExp(searcher, "i");
-        return searchColumns.some(
-          (column) => toString(item[column]).search(regexp) > -1
-        );
-      });
-    };
-    store.search.shouldCacheUpdate = true;
-    store.sort.shouldCacheUpdate = true;
-    store.page.shouldCacheUpdate = true;
-    return r;
-  }
-
-  function sort(
-    option: OneOrMore<keyof T> | ((a: T, b: T) => number),
-    order?: OneOrMore<"acs" | "desc" | "ascing" | "descing">
-  ) {
-    store.sort.method = (d) => {
-      d = d.concat();
-      if (typeof option === "function") {
-        return d.sort(option);
-      } else {
-        const options = toArray(option);
-        const orders = toArray(order);
-        const sorter = (a: any, b: any) =>
-          options
-            .map((o, i) => {
-              const dir = ["acs", "ascing", undefined].includes(orders[i])
-                ? 1
-                : -1;
-              return a[o] > b[o] ? dir : a[o] < b[o] ? -dir : 0;
-            })
-            .reduce((p, n) => (p ? p : n), 0);
-        d.sort(sorter);
-        return d;
-      }
-    };
-    store.sort.shouldCacheUpdate = true;
-    store.page.shouldCacheUpdate = true;
-    return r;
-  }
-
-  function page(current: number, size: number | "all" = "all") {
-    store.page.method = (d) => {
-      if (size === "all" || size <= 0) {
-        return {
-          current: d.concat(),
-          page: 1,
-          pageCount: 1,
-          total: d.length,
-        };
-      }
-      if (current < 1) {
-        current = 1;
-      }
-
-      return {
-        current: d.slice((current - 1) * size, current * size),
-        page: current,
-        total: d.length,
-        pageCount: Math.ceil(d.length / size),
-      };
-    };
-    store.page.shouldCacheUpdate = true;
-    return r;
-  }
-
-  function exec() {
-    Object.values(store).forEach((item) => {
-      if (item.shouldCacheUpdate) {
-        item.update();
-        item.shouldCacheUpdate = false;
-      }
-    });
-    return store.page.cache;
-  }
-
-  return r;
+  return processor;
 }
